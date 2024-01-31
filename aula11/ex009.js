6f71983cfc4d0fb3d35cdf3a592bca6f1e15a1f1
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-01-30 23:35:28 -0300

    pronto

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'frança'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil'){ /* Ser o país for Brasil ele vai usar esse comando, isso vale para o sina que você usa <, >, <=, >=, ==, !=*/
+    console.log('Você é Brasileiro!')
+}
+else{ // Ser o país for diferente ele vai usar esse comando
+    console.log('Você é Estrangeiro')
+}
