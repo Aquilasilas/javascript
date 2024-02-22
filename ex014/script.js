97ee408448386b7e5126a8a2dae13c1b6b4681b1
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-02-21 21:05:40 -0300

    novo

--- ex014/script.js
+++ ex014/script.js
@@ -5,4 +5,18 @@ var data = new Date()
 var hora = data.getHours()
 
 msg.innerHTML =`Agora são ${hora} horas.`
+if( hora >= 0 && hora < 12){
+     // BOM DIA!
+     img.src = 'dia.png'
+     document.body.style.background = '#ccd139'     
+}   else if(hora >= 12 && hora <= 18){
+     // BOA TARDE!
+     img.src = 'tarde.png'
+     document.body.style.background = '#b67034'
+    
+}   else {
+     // BOA NOITE!
+     img.src = 'noite.png'
+     document.body.style.background = '#06397b'
+}
 }
