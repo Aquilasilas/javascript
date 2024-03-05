cf4ad7b0eb0005b43f1db79756c97fd704f2a3d8
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-03-05 18:26:56 -0300

    js

--- ex015/script.js
+++ ex015/script.js
@@ -1,1 +1,12 @@
-window.alert('Olá!')
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = window.document.getElementById('txtano')
+   var res = window.document.getElementById('res')
+   if(fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert('[ERRO] Verifiqui os dados e tente novamente!')
+     }
+     else{
+        window.alert('Tudo ok')
+     }
+}
