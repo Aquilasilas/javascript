a78894a680239e0f5630bae3f493961d5cfdfdb1
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-03-06 21:18:59 -0300

    novo

--- ex015/script.js
+++ ex015/script.js
@@ -10,10 +10,44 @@ function verificar(){
         window.alert('Tudo ok')
      }
 
-     var fsex = document.getElementsByTagName('radsex')
+     var fsex = document.getElementsByName('radsex')
      var idade = ano - Number(fano.value)
      var gênero = ''
+     var img = document.createElement('img')
+     img.setAttribute('id', 'foto')
      if(fsex[0].checked){
-      gènero = 'Home'
+        gênero = 'Homem'
+        if( idade >= 0 && idade < 10 ) {
+         //criança
+         img.setAttribute('src', 'foto-crianca-hh.png')
+      } else if ( idade < 20) {
+         //Jovem
+         img.setAttribute('src', 'foto-jovem-hh.png')
+      } else if( idade < 50) {
+         //Adulto
+         img.setAttribute('src', 'foto-adulto-hh.png')
+      } else {
+         // Idoso 
+         img.setAttribute('src', 'foto-idoso-hh.png')
+      }
+   } else if (fsex[1].checked){
+      gênero = 'Mulher'
+      if( idade >= 0 && idade < 10 ) {
+         //criança
+         img.setAttribute('src', 'foto-crianca-mm.png')
+      } else if ( idade < 20) {
+         //Jovem
+         img.setAttribute('src', 'foto-jovem-mm.png')
+      } else if( idade < 50) {
+         //Adulto
+         img.setAttribute('src', 'foto-adulto-mm.png')
+      } else {
+         // Idoso 
+         img.setAttribute('src', 'foto-idoso-mm.png')
+      }
      }
+    
+     res.style.textAlign = 'center' // usa para centraliza o texto no js
+     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+     res.appendChild(img)// Usa para adisiona um elemento 
 }
