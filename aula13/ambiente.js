d45b44e7eb4d3edf65410ab852749fae5b5f36e0
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-03-07 21:50:06 -0300

    js

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+/*while (c <= 7){
+    console.log(`Passo ${c}`)
+    c++                    // é a var c + 1
+}*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+do {
+    console.log(`Passo ${c}`)
+    c++                    // é a var c + 1
+} while (c <= 6)
+
+
