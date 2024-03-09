301c88d4e0e01faf2ab81c70ae17708c2b1e9b5d
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-03-08 21:48:26 -0300

    js

---
+++ aula14/ambiente.js
@@ -0,0 +1,10 @@
+/*var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}*/
+console.log('Vai começa...')
+for (var c = 1; c <= 5; c++ ){
+    console.log(`Pode contar ${c}`)
+}
+console.log('FIM')      
--- ex014/ambiente.js
+++ ex014/ambiente.js
@@ -10,7 +10,7 @@ while (c <= 6) {
 /*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
-console.log('Tudo bem?')
+console.log('Tudo bem?') 
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
