dec98a681527ea178d0a24bac1f88fee2d817ced
Àquila Silas Jesus Silva <143562730+Aquilasilas@users.noreply.github.com>
2024-03-21 22:21:17 -0300

    ex js

---
+++ aula16/funçao.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+if(n%2 == 0){
+    return 'Par!'
+} else{
+    return 'Impar!'
+}
+}
+console.log(parimpar(4))
+/*ou*/
+/*let res = parimpar(4)
+console.log(`O número é ${res}`)*/
